7ce42cfee13b60c704f21d7aea3f29dd864e3379
Saleh-Alibrahim <s.albrahim@outlook.com>
2022-12-04 14:37:53 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
-import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Flex,
+  Heading,
+  Text,
+  VStack,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginForm from './components/login/LoginForm';
+import ProtectedRoute from './components/ProtectedRoute';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import TodoHome from './pages/TodoHome';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path='/' element={<TodoHome />} />
+          </Route>
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
+  );
+};
